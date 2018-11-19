71d4e8496474a4da22df70acb5342edf01d13a04
Daniele Roncaglioni <roncaglionidaniele>
2018-11-19 17:44:25 +0100

    Set up frontend

--- app/project/settings.py
+++ app/project/settings.py
@@ -134,8 +134,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = '/static-files/'
+MEDIA_URL = '/media-files/'
 STATIC_ROOT = '/static-files'
 MEDIA_ROOT = '/media-files'
 
