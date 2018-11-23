26fb33afb828fae55e619ceb0ea06a3a37abede6
Ian Stump <ianstump@gmail.com>
2018-11-23 16:40:24 +0100

    working upload

--- app/project/apps/file_storing/views.py
+++ app/project/apps/file_storing/views.py
@@ -1,10 +1,6 @@
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
-from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
-from .serializer import FileSerializer
-from rest_framework.response import Response
 
 
 class FileView(APIView):
