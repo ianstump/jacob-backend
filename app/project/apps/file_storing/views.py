ddba6f2258e96949c368b33d9bb67bf58c7f7cc2
Daniele Roncaglioni <roncaglionidaniele>
2018-11-26 09:13:58 +0100

    pdfs

--- app/project/apps/file_storing/views.py
+++ app/project/apps/file_storing/views.py
@@ -1,18 +1,18 @@
-from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 from rest_framework.views import APIView
-
+# from django.core.files.storage import FileSystemStorage
 from project.base.apps.tags.models import Pdf_documents
 
 
 class FileView(APIView):
     permission_classes = []
+
     def post(self, request, **kwargs):
         myfile = request.FILES['filepond']
-        fs = FileSystemStorage()
+        # fs = FileSystemStorage()
         # filename = fs.save(myfile.name, myfile)
         # file_name = f'/media-files/{filename}'
         instance = Pdf_documents(report=myfile)
         instance.save()
-        # print("filename", file_name)
+        # print("filename", file_name)s
         return HttpResponse("Working upload")
