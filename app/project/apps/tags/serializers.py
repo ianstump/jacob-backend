ecb5c0a0b475629f63f82d20708dd06603e6d216
Daniele Roncaglioni <roncaglionidaniele>
2018-11-27 09:43:09 +0100

    Fetch tags for dropdown, backend and frontend

--- app/project/apps/tags/serializers.py
+++ app/project/apps/tags/serializers.py
@@ -6,12 +6,10 @@ from project.base.apps.tags.models import Document_tags, Highlighted_text
 class TagsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document_tags
-        fields = ['name', 'pdf_documents']
+        fields = ['id', 'name', 'pdf_documents']
 
 
 class HighlightedTextSerializer(serializers.ModelSerializer):
     class Meta:
         model = Highlighted_text
         fields = ['selected_text']
-
-
