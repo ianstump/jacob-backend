2a1652efb675eaee8285dad10d0ee399d3288564
Ian Stump <ianstump@gmail.com>
2018-11-29 10:45:27 +0100

    added id in pdfs and cleaned text

---
+++ app/project/apps/annotations/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from project.apps.annotations.serializers import HighlightedTextSerializer
+
+
+class AnnotateText(APIView):
+    """
+    annotated text that posts text, with the respective pdf and tag
+    """
+
+    def post(self, request, **kwargs):
+        serializer = HighlightedTextSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            text = serializer.save()
+        return Response(HighlightedTextSerializer(text).data)
