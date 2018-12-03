5451514bfc3843d4a4140c2492cc7012fb1f6e13
Colin Wirz <cwirz@pyango.ch>
2018-12-03 14:45:10 +0100

    Refactor signals

--- app/project/base/apps/tags/config.py
+++ app/project/base/apps/tags/config.py
@@ -2,6 +2,9 @@ from django.apps import AppConfig
 
 
 class TagsAppConfig(AppConfig):
+    name = 'project.base.apps.tags'
+    verbose_name = _(u"tags")
+    label = "tags"
 
     def ready(self):
         from . import signals  # noqa
