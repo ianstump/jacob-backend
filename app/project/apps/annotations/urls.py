43575fc976d9f4119baf0ddf20260eb8a243349c
Ian Stump <ianstump@gmail.com>
2018-12-05 09:25:33 +0100

    models bim boom bam

--- app/project/apps/annotations/urls.py
+++ app/project/apps/annotations/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from project.apps.annotations.views import AnnotateText
+from project.apps.annotations.views import AnnotateText, GetAllHighlights
 
 urlpatterns = [
     path('', AnnotateText.as_view(), name='annotated-text'),
+    path('all/', GetAllHighlights.as_view(), name='get-annotated-text'),
+
 ]
