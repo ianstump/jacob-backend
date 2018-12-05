1ab1af151932d786f4d2d7e429548f8500bd125c
Daniele Roncaglioni <roncaglionidaniele>
2018-12-05 11:17:59 +0100

    merged document card changes and faysal frontend

--- app/project/apps/search/views.py
+++ app/project/apps/search/views.py
@@ -1,5 +1,4 @@
 from rest_framework.views import APIView
-from project.apps.search.serializer import PdfDocumentSerializer
 from project.base.apps.tags.documents import PdfFileDocument
 from rest_framework.response import Response
 
@@ -9,7 +8,9 @@ class SearchText(APIView):
         query = request.query_params.get('q')
         if query:
             searched_result = PdfFileDocument.search().query('match', text=query).highlight('text', type="plain",
-                fragment_size=500, number_of_fragments=5000).highlight_options(order='score', pre_tags=["<b class='red'>"], post_tags=["</b>"])
+                                                                                            fragment_size=500,
+                                                                                            number_of_fragments=5000).highlight_options(
+                order='score', pre_tags=["<b class='red'>"], post_tags=["</b>"])
             resp = searched_result.execute()
             resp_dict = resp.to_dict()
             return Response(resp_dict['hits'])
