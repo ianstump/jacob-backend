e5e6cd2ecebbece91ab25606a460dd6ace88193e
Daniele Roncaglioni <roncaglionidaniele>
2018-12-06 15:59:09 +0100

    linting

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python manage.py migrate
+python manage.py loaddata ./fixtures/db.json
 python manage.py collectstatic --noinput
 python manage.py search_index --rebuild
 python manage.py runserver 0.0.0.0:8000
